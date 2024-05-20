ea335f5226af08ea290bd53fe3c888219e12d860
Bruno Henrique <brunohc2016@gmail.com>
2024-05-20 15:20:48 -0300

    update

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -22,47 +22,111 @@ export function MapRoute() {
     zoom: 13,
   });
 
-  const [start, setStart] = useState([-48.005312, -15.838992]); // [lng, lat]
-  const [end, setEnd] = useState([-48.04581, -15.835338]); // [lng, lat]
-  const [driverLocation, setDriverLocation] = useState<number[]>([
-    start[0],
-    start[1],
-  ]); // [lng, lat]
-
+  const start = useRef<number[]>([-48.005312, -15.838992]); // [lng, lat]
+  const end = useRef<number[]>([-48.04581, -15.835338]); // [lng, lat]
+  const endTest = useRef<number[]>([-48.04581, -15.8]); // [lng, lat]
+  const driverLocation = useRef<number[]>([start.current[0], start.current[1]]);
+  
   const coords = useRef<number[][]>([]);
+  const coordsTest = useRef<number[][]>([]);
 
-  const getRoute = async () => {
+  const getRoute = async (startCoords: number[]) => {
     const response = await fetch(
-      `https://router.project-osrm.org/route/v1/driving/${start[0]},${start[1]};${end[0]},${end[1]}?overview=full&geometries=geojson`
+      `https://router.project-osrm.org/route/v1/driving/${startCoords[0]},${startCoords[1]};${end.current[0]},${end.current[1]}?overview=full&geometries=geojson`
+    );
+
+    const data = await response.json();
+    const firstCoords = data.routes[0].geometry.coordinates;
+    console.log(firstCoords);
+
+    coords.current = firstCoords;
+  };
+
+  const getWrongRoute = async (startCoords: number[], endCoords: number[]) => {
+    const response = await fetch(
+      `https://router.project-osrm.org/route/v1/driving/${startCoords[0]},${startCoords[1]};${endCoords[0]},${endCoords[1]}?overview=full&geometries=geojson`
     );
     const data = await response.json();
     const firstCoords = data.routes[0].geometry.coordinates;
-    coords.current = firstCoords;
-    console.log("firstCoords", firstCoords);
+    coordsTest.current = firstCoords;
   };
 
   const updateRoute = (idx: number) => {
-    const newCoords = coords.current.splice(idx);
+    const newCoords = coords.current.slice(idx);
     coords.current = newCoords;
-    console.log("rota atualizada", newCoords[idx]);
   };
 
   const debounce = (delay: number) => {
     let currentIndex = 0;
 
     const intervalId = setInterval(() => {
-      setDriverLocation(coords.current[currentIndex]);
-      updateRoute(currentIndex);
-      // console.log(`coords[${currentIndex}]${coords.current[currentIndex]}`);
+      driverLocation.current = coords.current[1];
+      updateRoute(1);
 
       currentIndex = currentIndex + 1;
-      if (currentIndex > coords.current.length) {
+      console.log(currentIndex, coords.current.length);
+
+      // corrigir: ele esta parando antes de chagar ao destinoç
+      
+      if (coords.current.length === 1) {
         clearInterval(intervalId);
       }
     }, delay);
 
     return () => clearInterval(intervalId);
   };
+
+  const updateRouteLocation = () => {
+    const tolerance = 0.0001; 
+  
+    const isDriverOnRoute = coords.current.some((coord) => {
+      const latDiff = Math.abs(coord[1] - driverLocation.current[1]);
+      const lngDiff = Math.abs(coord[0] - driverLocation.current[0]);
+  
+      return latDiff < tolerance && lngDiff < tolerance;
+    });
+  
+    // console.log("isDriverOnRoute", isDriverOnRoute);
+  
+    if (!isDriverOnRoute) {
+      getRoute(driverLocation.current);
+    }
+  };
+  
+
+  // const updateRouteLocation = () => {
+  //   const isDriverOnRoute = coords.current.some((coord) => {
+  //     console.log("coord", coord[0], coord[1])
+  //     console.log(
+  //       "driver coord",
+  //       driverLocation.current[0],
+  //       driverLocation.current[1]
+  //     );
+  //     coord[0] === driverLocation.current[0] &&
+  //       coord[1] === driverLocation.current[1];
+  //   });
+
+  //   console.log("isDriverOnRoute", isDriverOnRoute);
+
+    // if (!isDriverOnRoute) {
+    //   getRoute(driverLocation.current);
+    // }
+  // };
+
+  useEffect(() => {
+    updateRouteLocation();
+  }, [driverLocation.current]);
+
+  useEffect(() => {
+    getRoute(start.current);
+    getWrongRoute(start.current, endTest.current);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      debounce(100);
+    }, 3000);
+  }, []);
 
   const geojson = {
     type: "FeatureCollection",
@@ -91,16 +155,6 @@ export function MapRoute() {
     },
   };
 
-  useEffect(() => {
-    getRoute();
-  }, []);
-
-  useEffect(() => {
-    setTimeout(() => {
-        debounce(1000);
-    }, 3000);
-  }, []);
-
   return (
     <>
       <Map
@@ -114,15 +168,18 @@ export function MapRoute() {
           <Layer {...lineStyle} />
         </Source>
 
-        <Marker longitude={start[0]} latitude={start[1]}>
+        <Marker longitude={start.current[0]} latitude={start.current[1]}>
           <img src={pickUp} width={48} height={48} />
         </Marker>
 
-        <Marker longitude={end[0]} latitude={end[1]}>
+        <Marker longitude={end.current[0]} latitude={end.current[1]}>
           <img src={finish} width={48} height={48} />
         </Marker>
 
-        <Marker longitude={driverLocation[0]} latitude={driverLocation[1]}>
+        <Marker
+          longitude={driverLocation.current[0]}
+          latitude={driverLocation.current[1]}
+        >
           <img src={basicCar} width={48} height={48} />
         </Marker>
 
